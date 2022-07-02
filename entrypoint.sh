ca7101b8a1158f5b891911d7c9818046f5af8b9b
Grahame White <57143548+grahame-student@users.noreply.github.com>
2022-07-02 19:42:33 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,14 +9,8 @@ cd "$1" || exit 1
 cmake CMakeLists.txt
 make || exit 2
 
-# list contents of cmakelists directory
-ls -la "$1/CMakeLists"
-
 # execute the unit tests
 ./unit_tests || exit 3
 
 # generate a coverage report
 gcovr --branches --xml-pretty -r . >> gcovr-report.xml
-
-# Show the resulting files
-ls -la

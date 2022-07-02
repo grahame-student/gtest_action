eca4cdfff73353d66c16cc681abdf1d8dd663af0
Grahame White <57143548+grahame-student@users.noreply.github.com>
2022-07-02 17:14:49 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,17 @@
 # Args
 # 1 - path to directory with test cases
 
-# Copy CMakeLists.txt to $1
+# Build the unit tests
 cp /CMakeLists.txt "$1"
 cd "$1" || exit 1
 cmake CMakeLists.txt
 make
+
+# execute the unit tests
+./unit_tests
+
+# generate a coverage report
+gcovr --branches --xml-pretty -r . >> gcovr-report.xml
+
+# Show the resulting files
 ls -la

01e4171872cec22a1b8c6f36c695c6ea493c2de9
Grahame White <57143548+grahame-student@users.noreply.github.com>
2022-07-02 20:23:59 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,9 @@
 # Args
 # 1 - path to directory with test cases
 
+# Record starting directory
+cwd="$PWD"
+
 # Build the unit tests
 cp /CMakeLists.txt "$1"
 cd "$1" || exit 1
@@ -10,6 +13,8 @@ cmake CMakeLists.txt
 make || exit 2
 
 # execute the unit tests
+cd "$cwd"
+cp "$1/unit_tests" "$cwd"
 ./unit_tests || exit 3
 
 # generate a coverage report

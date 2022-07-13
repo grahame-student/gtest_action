bf7da433ab2dd8339b4c560894dc4e801e010138
Grahame White <57143548+grahame-student@users.noreply.github.com>
2022-07-13 18:11:36 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,26 +5,18 @@
 
 # Split the delimited string into an array of paths
 IFS=';' read -ra paths <<< "$1"
+
 for i in "${paths[@]}"; do
-  echo "$i"
+  cp /CMakeLists.txt "$i"
+  pushd "$i" || exit 1
+  cmake CMakeLists.txt
+  make || exit 2
+
+  # execute the unit tests
+  popd || exit 3
+  "./$i/unit_tests" || exit 4
+
+  # generate a coverage report
+  uuid=$(uuidgen)
+  gcovr --branches --xml-pretty -r . >> "$uuid-report.xml"
 done
-
-# Record starting directory
-project_root="$PWD"
-
-# The following will need to wrapped so that multiple unit test
-# projects can be iterated through
-
-# Build the unit tests
-cp /CMakeLists.txt "$1"
-cd "$1" || exit 1
-cmake CMakeLists.txt
-make || exit 2
-
-# execute the unit tests
-cd "$project_root" || exit 3
-"./$1/unit_tests" || exit 4
-
-# generate a coverage report
-uuid=$(uuidgen)
-gcovr --branches --xml-pretty -r "$project_root" >> "$uuid-report.xml"
